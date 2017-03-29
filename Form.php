418a9bc485804b380ab77e29d11a3baecadfd2cb
nalifah <nalifah10.na@gmail.com>
2017-03-29 21:50:04 +0700

    first commit

---
+++ Form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!-----INPUT---->
+<body>
+<form action="" method="get" target="_self" name="frm_penjualan">
+<table border="1" align="center" width="245">
+    	<th colspan="2">Penjualan</th>
+    </tr>
+    	<td align="center">Nama Barang</td>
+        <td><input type="text" maxlength="200" name="nama" size="40"></td>
+    </tr>
+    	<td align="center">Harga</td>
+        <td><input type="text" maxlength="100" name="harga" size="40"></td>
+    </tr>
+    	<td align="center">Quantty</td>
+        <td><input type="text" maxlength="100" name="qty" size="40"></td>
+    </tr>
+</html>
